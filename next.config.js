a21e15bf0aa15ff576c72097976b651626f8d2a1
Dayo Akinbami <akinbamizacheus@gmail.com>
2020-05-27 12:53:26 +0100

    changed config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const withCSS = require('@zeit/next-css')
 module.exports = withCSS({
+	distDir: '_next',
   target: 'serverless', // <- add here
   cssModules: true
   // assetPrefix: "https://s3.amazonaws.com/abc-static-assets",
