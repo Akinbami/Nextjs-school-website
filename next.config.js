8b40c30f56a74b16b99f9ba9fcaf1ce0fd00c4a2
Dayo Akinbami <akinbamizacheus@gmail.com>
2020-05-27 13:32:14 +0100

    changed build directory to build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const withCSS = require('@zeit/next-css')
 module.exports = withCSS({
-	distDir: '_next',
+	distDir: 'build',
   target: 'serverless', // <- add here
   cssModules: true
   // assetPrefix: "https://s3.amazonaws.com/abc-static-assets",
