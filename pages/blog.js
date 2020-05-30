a292968234270b662c0943e4b63efc45c8424e07
Dayo Akinbami <akinbamizacheus@gmail.com>
2020-05-30 18:42:14 +0100

    removed test form in blog page

--- pages/blog.js
+++ pages/blog.js
@@ -27,16 +27,7 @@ const Blog = (props) => {
             </div>
         </div>
       </div>
-        <div className="form-group">
-          <label htmlFor="exampleInputEmail1">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-          <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-        </div>
-        <div className="form-group">
-          <label htmlFor="exampleInputPassword1">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" />
-        </div>
-        <button type="button" onClick={e=>console.log("this is a new form ")} className="btn btn-primary">Submit</button>
+
       <div className="section">
         <div className="content-wrap">
           <div className="container">       
